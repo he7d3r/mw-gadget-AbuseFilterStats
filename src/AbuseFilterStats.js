94dc58fce9cf81dff867e1c96798b0274cd90b3d
Helder <he7d3r@gmail.com>
2013-07-11 16:33:35 +0000

    Sort by the number of detections
    Also, alow choosing the month number and do not get more data than necessary from API.

--- src/AbuseFilterStats.js
+++ src/AbuseFilterStats.js
@@ -3,19 +3,17 @@
  * @author: [[User:Helder.wiki]]
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/AbuseFilterStats.js]] ([[File:User:Helder.wiki/Tools/AbuseFilterStats.js]])
  */
-/*jshint browser: true, camelcase: false, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, laxbreak: true, maxlen: 120, evil: true, onevar: true */
+/*jshint browser: true, camelcase: false, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, laxbreak: true, devel: true, maxlen: 120, evil: true, onevar: true */
 /*global jQuery, mediaWiki */
 ( function ( mw, $ ) {
 'use strict';
 
 var api = new mw.Api(),
-	stats = [
-		[ 'Filtro', 'Detecções', 'Conferidas', '%', 'Falsos positivos', '% das conferidas' ]
-	],
+	stats = [],
 	verificationPages = [];
 
 function printTable( table ){
-	var i, checked, errors, hits,
+	var $target, i, checked, errors, hits, id,
 		wikicode = [
 			'{| class="wikitable sortable plainlinks"',
 			'|+ Controle de qualidade dos filtros de edição',
@@ -27,14 +25,21 @@ function printTable( table ){
 			'! data-sort-type="number" | Falsos<br />positivos',
 			'! data-sort-type="number" | % das<br />conferidas'
 		].join( '\n' );
-	for ( i = 1; i < table.length; i++ ){
+	table.sort( function( a, b ) {
+		return b.hits - a.hits;
+	} );
+	for ( i = 0; i < table.length; i++ ){
+		if( !table[i] ){
+			continue;
+		}
+		id = table[i].id;
 		hits = table[i].hits;
 		checked = table[i].checked;
 		errors = table[i].errors;
 		wikicode += '\n|-\n| ' + [
-			'[[Especial:Filtro de abusos/' + i + '|' + i + ']]',
-			'[{{fullurl:Especial:Registro de abusos|wpSearchFilter=' + i + '}} ' + hits + ']',
-			'[[WP:Filtro de edições/Falsos positivos/Filtro ' + i + '|' + checked + ']]',
+			'[[Especial:Filtro de abusos/' + id + '|' + id + ']]',
+			'[{{fullurl:Especial:Registro de abusos|wpSearchFilter=' + id + '}} ' + hits + ']',
+			'[[WP:Filtro de edições/Falsos positivos/Filtro ' + id + '|' + checked + ']]',
 			hits === 0
 				? '-'
 				: ( 100 * checked / hits ).toFixed( 1 ) + '%',
@@ -47,38 +52,58 @@ function printTable( table ){
 		].join( ' || ' );
 	}
 	wikicode += '\n|}';
-	
-	$( '#mw-content-text' )
-		.prepend(
-			'<b>O código da tabela atualizada é apresentado abaixo:</b><br /><br />' +
-			'<textarea cols="80" rows="10" style="width: 100%; font-family: monospace; line-height: 1.5em;">' +
-				mw.html.escape( wikicode ) +
-			'</textarea>'
-		);
+
+	$target = $( '#abuse-filter-stats-result' );
+	if ( !$target.length ){
+		$target = $( '<div id="abuse-filter-stats-result">' ).prependTo( '#mw-content-text' );
+	}
+	$target.empty().append(
+		'<b>O código da tabela atualizada é apresentado abaixo:</b><br /><br />' +
+		'<textarea cols="80" rows="10" style="width: 100%; font-family: monospace; line-height: 1.5em;">' +
+			mw.html.escape( wikicode ) +
+		'</textarea>'
+	);
 	$.removeSpinner( 'spinner-filter-stats' );
 	// $( 'table.sortable' ).tablesorter();
 }
 
 function getAbuseFilterStats(){
-	var param,
+	var param, firstDay, lastDay, getLog,
 		d = new Date(),
-	firstDay = new Date(d.getFullYear(), d.getMonth(), 1),
+		month = prompt(
+			'Deseja obter as estatísticas referentes a que mês?' +
+				' (forneça um número natural de 1 a 12)',
+			d.getMonth() + 1
+		);
+	if ( month === null ){
+		return;
+	}
+	month = parseInt( month, 10 );
+	if ( isNaN( month ) || month < 0 || 11 < month ){
+		alert( 'Operação cancelada! O mês fornecido não é válido.' );
+		return;
+	}
+	firstDay = new Date(d.getFullYear(), month - 1, 1);
 	// end of the current month
-	lastDay = new Date(d.getFullYear(), d.getMonth() + 1, 0, 23, 59, 59),
+	lastDay = new Date(d.getFullYear(), month, 0, 23, 59, 59);
 	// end of the first week of the current month
-	// lastDay = new Date(d.getFullYear(), d.getMonth(), 7, 23, 59, 59),
+	// lastDay = new Date(d.getFullYear(), d.getMonth(), 7, 23, 59, 59);
 	getLog = function( queryContinue ){
 		if( queryContinue ){
 			$.extend( param, queryContinue );
 		}
-			api.get( param )
+		api.get( param )
 		.done( function ( data ) {
 			var i, list, log, match;
 			list = data.query.abuselog;
 			for ( i = 0; i < list.length; i++ ){
 				log = list[i];
+				if ( log.filter_id === '' ){
+					continue;
+				}
 				if ( stats[ log.filter_id ] === undefined ){
 					stats[ log.filter_id ] = {
+						id: log.filter_id,
 						hits: 0,
 						checked: 0,
 						errors: 0
@@ -96,21 +121,22 @@ function getAbuseFilterStats(){
 					}
 				}
 			}
-				if( data[ 'query-continue' ] ){
+			if( data[ 'query-continue' ] ){
 				getLog( data[ 'query-continue' ].abuselog );
 			} else {
 				for ( i = 1; i < stats.length; i++ ){
 					if ( !stats[i] ){
 						stats[i] = {
+							id: i,
 							hits: 0,
 							checked: 0
 						};
-						}
 					}
+				}
 				printTable( stats );
 			}
 		} )
-			.fail( function ( data ) {
+		.fail( function ( data ) {
 			mw.log( data.query );
 			$.removeSpinner( 'spinner-filter-stats' );
 		} );
@@ -122,7 +148,7 @@ function getAbuseFilterStats(){
 		// aflfilter: 123,
 		aflstart: firstDay.toISOString(),
 		aflend: lastDay.toISOString(),
-		aflprop: 'ids|filter|user|title|action|result|timestamp|hidden|revid', // removed: details|ip
+		aflprop: 'ids', // |filter|user|title|action|result|timestamp|hidden|revid|details|ip
 		afldir: 'newer'
 	};
 	getLog();
